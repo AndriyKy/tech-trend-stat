74fa903a120dea7de2e03307534b82dc8265e549
andriyky <akrechovetskiy@gmail.com>
2024-01-13 14:58:49 +0200

    feat: add pydantic item

--- techtrendscrape/items.py
+++ techtrendscrape/items.py
@@ -3,10 +3,21 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from datetime import datetime
+from typing import Literal
+
+from pydantic import BaseModel, Field, NonNegativeInt
 
 
-class TechtrendscrapeItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class VacancyItem(BaseModel):
+    source: Literal["djinni"] = "djinni"
+    category: str = Field(min_length=1)
+    company_name: str = Field(min_length=1)
+    company_type: str | None = Field(min_length=1)
+    # TODO: Integrate smart search (nltk, TextBlob).
+    # technologies: conlist(str, min_length=1)
+    description: str
+    years_of_experience: NonNegativeInt
+    publication_date: datetime
+    views: NonNegativeInt | None
+    applications: NonNegativeInt | None
